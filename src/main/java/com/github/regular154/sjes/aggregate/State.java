30b2dd14bc620905fe3373a17d6d0222b733fa26
Siroshtan <sergey.siroshtan@signanthealth.com>
2025-02-21 16:45:52 +0100

    Revert "return list of events as the result of command execution"
    This reverts commit e5cf8ff65f8fd54f21a7c1d73e224e4538af2288.

--- src/main/java/com/github/regular154/sjes/aggregate/State.java
+++ src/main/java/com/github/regular154/sjes/aggregate/State.java
@@ -1,10 +1,8 @@
 package com.github.regular154.sjes.aggregate;
 
-import java.util.List;
-
 public abstract class State<E, C> {
 
     public abstract String getStateName();
-    public abstract List<E> processCommand(C command, long sequence) throws DomainException;
+    public abstract E processCommand(C command, long sequence) throws DomainException;
     public abstract State<E, C> applyEvent(E event);
 }
--- src/test/java/com/github/regular154/sjes/aggregate/setup/domain/account/state/Pending.java
+++ src/test/java/com/github/regular154/sjes/aggregate/setup/domain/account/state/Pending.java
@@ -10,7 +10,6 @@ import com.github.regular154.sjes.aggregate.setup.domain.account.event.AccountFu
 import com.github.regular154.sjes.aggregate.setup.domain.account.event.AccountFundsWithdrawn;
 import com.github.regular154.sjes.aggregate.setup.domain.account.event.AccountNameChanged;
 
-import java.util.List;
 import java.util.UUID;
 
 
@@ -27,26 +26,26 @@ public class Pending extends AccountState {
     }
 
     @Override
-    public List<AccountEvent> processCommand(AccountCommand command, long sequence) throws DomainException {
+    public AccountEvent processCommand(AccountCommand command, long sequence) throws DomainException {
         return switch (command) {
             case Deposit deposit -> {
                 if (deposit.getAmount() <= 0) {
                     throw new DomainException("Deposit amount must be greater than 0");
                 }
                 var resultedBalance = balance + deposit.getAmount();
-                yield List.of(new AccountFundsDeposited(
+                yield new AccountFundsDeposited(
                         UUID.randomUUID().toString(),
                         command.getAggregateId(),
                         sequence,
                         deposit.getAmount(),
-                        resultedBalance));
+                        resultedBalance);
             }
             case Withdraw ignored -> throw new DomainException("Account is not active");
-            case ChangeAccountName changeAccountName -> List.of(new AccountNameChanged(
+            case ChangeAccountName changeAccountName -> new AccountNameChanged(
                     UUID.randomUUID().toString(),
                     changeAccountName.getAggregateId(),
                     sequence,
-                    changeAccountName.getName()));
+                    changeAccountName.getName());
             default -> throw new IllegalStateException("Unexpected command");
         };
     }
